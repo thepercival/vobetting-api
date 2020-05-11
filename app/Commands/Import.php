ed7acbc6e5daf3794cd3608158924377c8ae9876
thepercival <coendunnink@gmail.com>
2020-05-11 22:32:59 +0200

    add TheOddsApi

--- app/Commands/Import.php
+++ app/Commands/Import.php
@@ -13,6 +13,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use VOBetting\Attacher\Bookmaker\Repository as BookmakerAttacherRepository;
 use VOBetting\BetLine\Repository as BetLineRepository;
 use VOBetting\ExternalSource\Matchbook;
+use VOBetting\ExternalSource\TheOddsApi;
 use VOBetting\LayBack\Repository as LayBackRepository;
 use Voetbal\Attacher\Game\Repository as GameAttacherRepository;
 use Voetbal\Attacher\Place\Repository as PlaceAttacherRepository;
@@ -120,7 +121,7 @@ class Import extends Command
             $this->importGames(SofaScore::NAME);
         }
         if ($input->getOption("laybacks")) {
-            $this->importLayBacks([Betfair::NAME, Matchbook::NAME]);
+            $this->importLayBacks([Betfair::NAME, Matchbook::NAME, TheOddsApi::NAME]);
         }
         return 0;
     }
